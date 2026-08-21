fbf75815af74538a23a267c827b39635e64bc6a9
wzmwayne <wzmwayne@users.noreply.github.com>
2026-08-21 12:44:41 +0000

    fix: add missing IXposedHookLoadPackage stub

---
+++ app/src/main/java/de/robv/android/xposed/IXposedHookLoadPackage.java
@@ -0,0 +1,8 @@
+package de.robv.android.xposed;
+
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+// Compile-time stub; the real class is provided by LSPosed at runtime.
+public interface IXposedHookLoadPackage {
+    void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable;
+}
